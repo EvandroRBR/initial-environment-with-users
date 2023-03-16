85fd9460306a7f1ba9272b581e3ac6b440940a2f
EvandroRBR <evandro.braga@beedoo.com.br>
2023-03-16 18:13:17 -0300

    Create Users Unit Tests

--- src/use-cases/create-user.spec.ts
+++ src/use-cases/create-user.spec.ts
@@ -1,7 +1,8 @@
+import { compare } from 'bcryptjs';
 import { beforeEach, describe, expect, it } from 'vitest';
 
+import { UserAlreadyExistsError } from './errors/user-already-exists-error';
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository';
-
 import { CreateUserUseCase } from './create-user';
 
 let usersRepository: InMemoryUsersRepository;
@@ -21,5 +22,36 @@ describe('Create User Use Case', () => {
     });
 
     expect(user.id).toEqual(expect.any(String));
+  });
+
+  it('should not be able to register with the same email twice', async () => {
+    await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    await expect(() =>
+      sut.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
+  });
+
+  it('should be hash user password upon registration', async () => {
+    const { user } = await sut.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      '123456',
+      user.password_hash,
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
   });
 });
